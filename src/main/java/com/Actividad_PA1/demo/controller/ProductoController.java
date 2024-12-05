8f1dd5a7e66f2e0f3f9cad8089b2f35c7eba7d4b
Russel FS <russelfloressolano900@gmail.com>
2024-12-04 20:47:08 -0500

    parcia practical 1

---
+++ src/main/java/com/Actividad_PA1/demo/controller/ProductoController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Actividad_PA1.demo.controller;
+
+/**
+ *
+ * @author LAB_P03
+ */
+ 
+import com.Actividad_PA1.demo.model.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductoController {
+
+    @GetMapping("/")
+    public String viewProducto(Model model) {
+
+        model.addAttribute("user", new user());
+        return "index";
+    }
+
+    @PostMapping("/register")
+    public String successProducto(@ModelAttribute user user, Model model) {
+
+        model.addAttribute("user", user);
+
+        if (user.getName().equals("admin") && user.getPassword().equals("admin")) {
+
+            return "success";
+        }
+        else{ 
+            return "error";
+        }
+    }
+}
